069e752a653f7f73f13a8c2ef43944781e0594b0
Will Schaller <willschaller@gmail.com>
2018-06-21 11:56:05 +0200

    Stack data structure implemented, tests passing

--- starter-code/src/StackDataStructure.js
+++ starter-code/src/StackDataStructure.js
@@ -1,2 +1,34 @@
-function StackDataStructure () {
+function StackDataStructure() {
+  this.stackControl = [];
+  this.MAX_SIZE = 5;
 }
+
+StackDataStructure.prototype.isEmpty = function() {
+  if (this.stackControl.length === 0) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+StackDataStructure.prototype.canPush = function() {
+  if (this.stackControl.length >= this.MAX_SIZE) {
+    return false;
+  }
+  return true;
+};
+
+StackDataStructure.prototype.push = function(element) {
+  if (this.stackControl.length >= this.MAX_SIZE) {
+    return "Stack Overflow";
+  }
+  this.stackControl.push(element);
+  return this.stackControl;
+};
+
+StackDataStructure.prototype.pop = function() {
+  if (this.stackControl.length === 0) {
+    return "Stack Underflow";
+  }
+  return this.stackControl.pop();
+};
